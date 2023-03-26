862c24ca75d46505103a56f09c6ecfb61751bb20
Hugo <26794193+hugovntr@users.noreply.github.com>
2023-03-26 12:50:35 +0200

    fix (blog layout): black-out header

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -4,14 +4,16 @@ import Link from "next/link";
 export default function Layout(props: { children: ReactNode }) {
     return (
         <>
-            <header className="container py-4">
-                <Link
-                    href={"/"}
-                    className="inline-flex items-baseline font-title text-2xl font-bold"
-                >
-                    <span>Hugo Ventura</span>
-                    <span className="ml-1 block h-2 w-2 rounded-full bg-emerald-400" />
-                </Link>
+            <header className="bg-black text-white">
+                <div className="container py-4">
+                    <Link
+                        href={"/"}
+                        className="inline-flex items-baseline font-title text-2xl font-bold"
+                    >
+                        <span>Hugo Ventura</span>
+                        <span className="ml-1 block h-2 w-2 rounded-full bg-emerald-400" />
+                    </Link>
+                </div>
             </header>
             {props.children}
         </>
