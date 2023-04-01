5c76d88d4dfd15223dd8e47b8af134ea33b90791
Hugo <26794193+hugovntr@users.noreply.github.com>
2023-04-01 13:34:22 +0200

    feat: add social links to blog navbar

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -5,7 +5,7 @@ export default function Layout(props: { children: ReactNode }) {
     return (
         <>
             <header className="bg-black text-white">
-                <div className="container py-4">
+                <div className="container flex items-center justify-between py-4">
                     <Link
                         href={"/"}
                         className="inline-flex items-baseline font-title text-2xl font-bold"
@@ -13,6 +13,46 @@ export default function Layout(props: { children: ReactNode }) {
                         <span>Hugo Ventura</span>
                         <span className="ml-1 block h-2 w-2 rounded-full bg-emerald-400" />
                     </Link>
+
+                    <div className="flex items-center gap-1">
+                        <Link
+                            className="flex items-center p-2 text-gray-300 transition-colors hover:text-emerald-400"
+                            href={"https://github.com/hugovntr"}
+                            rel="nofollow noopener"
+                            target="_blank"
+                        >
+                            <svg
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="h-6 w-6"
+                            >
+                                <path
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                    d="M12 2C6.477 2 2 6.463 2 11.97c0 4.404 2.865 8.14 6.839 9.458.5.092.682-.216.682-.48 0-.236-.008-.864-.013-1.695-2.782.602-3.369-1.337-3.369-1.337-.454-1.151-1.11-1.458-1.11-1.458-.908-.618.069-.606.069-.606 1.003.07 1.531 1.027 1.531 1.027.892 1.524 2.341 1.084 2.91.828.092-.643.35-1.083.636-1.332-2.22-.251-4.555-1.107-4.555-4.927 0-1.088.39-1.979 1.029-2.675-.103-.252-.446-1.266.098-2.638 0 0 .84-.268 2.75 1.022A9.606 9.606 0 0112 6.82c.85.004 1.705.114 2.504.336 1.909-1.29 2.747-1.022 2.747-1.022.546 1.372.202 2.386.1 2.638.64.696 1.028 1.587 1.028 2.675 0 3.83-2.339 4.673-4.566 4.92.359.307.678.915.678 1.846 0 1.332-.012 2.407-.012 2.734 0 .267.18.577.688.48C19.137 20.107 22 16.373 22 11.969 22 6.463 17.522 2 12 2z"
+                                ></path>
+                            </svg>
+                        </Link>
+                        <Link
+                            className="flex items-center p-2 text-gray-300 transition-colors hover:text-emerald-400"
+                            href={"https://twiter.com/hugovntr"}
+                            rel="nofollow noopener"
+                            target="_blank"
+                        >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="currentColor"
+                                className="h-6 w-6"
+                            >
+                                <path
+                                    d="M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"
+                                    fillRule="evenodd"
+                                    clipRule="evenodd"
+                                ></path>
+                            </svg>
+                        </Link>
+                    </div>
                 </div>
             </header>
             {props.children}
