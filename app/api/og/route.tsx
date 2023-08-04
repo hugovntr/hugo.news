a2c365dac8d0bac7026d780ee10b11a75acc3db0
hugovntr <26794193+hugovntr@users.noreply.github.com>
2023-08-04 20:10:49 +0200

    feat (gallery): Zoomable images with dynamic content

--- app/api/og/route.tsx
+++ app/api/og/route.tsx
@@ -3,9 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import { allBlogs } from "contentlayer/generated";
 import Background from "public/og.jpg";
 
-export const config = {
-    runtime: "edge",
-};
+export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
     const { searchParams } = request.nextUrl;
