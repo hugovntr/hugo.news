8ec56d5a823b853998a69e0c2d9c8ac7aa550c31
hugovntr <26794193+hugovntr@users.noreply.github.com>
2023-08-08 08:22:43 +0200

    feat: leverage `<Suspense />` for image modal

--- app/@modal/(.)gallery/images/[id]/page.tsx
+++ app/@modal/(.)gallery/images/[id]/page.tsx
@@ -4,6 +4,9 @@ import { PromptPopoverContent } from "@/app/prompt.client";
 import { Copyright } from "@/components/copyright";
 import { Dialog, DialogContent } from "./modal";
 import { ImageCollections } from "@/components/collections.server";
+import { FC, Suspense } from "react";
+import { FormData } from "next/dist/compiled/@edge-runtime/primitives";
+import { Skeleton } from "@/components/ui/skeleton";
 
 interface PageProps {
     params: {
@@ -14,35 +17,58 @@ interface PageProps {
 export const revalidate = 120;
 
 export default async function Page({ params }: PageProps) {
-    const { properties } = await getImage(params.id);
-    const infos = getImageInfos(properties);
-    if (!infos) return null;
-    const { prompt, share, title, url, collectionIds } = infos;
     return (
         <Dialog>
             <DialogContent className="max-w-screen flex h-auto max-h-screen w-full gap-0 overflow-hidden p-0 md:max-h-[calc(100vh_-_2rem)] md:w-max md:max-w-[calc(100vw_-_2rem)]">
-                <div
-                    className="max-h-screen"
-                    style={{
-                        aspectRatio: prompt.aspectRatio.split(":").join("/"),
-                    }}
-                >
-                    <Image
-                        src={url}
-                        alt={title}
-                        height={prompt.height}
-                        width={prompt.width}
-                    />
-                </div>
-                <div className="hidden w-full max-w-xs gap-4 border-l border-border p-4 pt-12 lg:flex lg:flex-col">
-                    <p className="mb-2 font-title text-lg font-semibold">
-                        {title}
-                    </p>
-                    <ImageCollections ids={collectionIds} />
-                    {share && <PromptPopoverContent {...prompt} />}
-                    <Copyright className="mt-auto" />
-                </div>
+                <Suspense fallback={<Fallback />}>
+                    <Data id={params.id} />
+                </Suspense>
             </DialogContent>
         </Dialog>
     );
 }
+
+const Fallback: FC = () => (
+    <>
+        <div className="overflow-hidden" style={{ aspectRatio: 2 / 3 }}>
+            <Skeleton style={{ width: 896, height: 1344 }} />
+        </div>
+        <div className="hidden w-full max-w-xs gap-4 border-l border-border p-4 pt-12 lg:flex lg:flex-col">
+            <Skeleton className="h-4 w-full" />
+            <Skeleton className="h-4 w-2/3" />
+            <Skeleton className="h-4 w-3/4" />
+            <Skeleton className="mt-auto h-4 w-2/3" />
+        </div>
+    </>
+);
+
+const Data: FC<{ id: string }> = async ({ id }) => {
+    const { properties } = await getImage(id);
+    const infos = getImageInfos(properties);
+    if (!infos) return null;
+    const { prompt, share, title, url, collectionIds } = infos;
+
+    return (
+        <>
+            <div
+                className="max-h-screen"
+                style={{
+                    aspectRatio: prompt.aspectRatio.split(":").join("/"),
+                }}
+            >
+                <Image
+                    src={url}
+                    alt={title}
+                    height={prompt.height}
+                    width={prompt.width}
+                />
+            </div>
+            <div className="hidden w-full max-w-xs gap-4 border-l border-border p-4 pt-12 lg:flex lg:flex-col">
+                <p className="mb-2 font-title text-lg font-semibold">{title}</p>
+                <ImageCollections ids={collectionIds} />
+                {share && <PromptPopoverContent {...prompt} />}
+                <Copyright className="mt-auto" />
+            </div>
+        </>
+    );
+};
