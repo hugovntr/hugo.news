ae946db1b1201a3a0a5ddf515de37a8181607a8f
hugovntr <26794193+hugovntr@users.noreply.github.com>
2023-11-28 21:01:22 +0100

    prod-fix (web): force add submodule during build

--- submodule.js
+++ submodule.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const { spawn } = require("node:child_process");
 
 const GIT_MODULES = ".gitmodules";
 
@@ -7,4 +8,18 @@ if (fs.existsSync(GIT_MODULES)) return;
 fs.writeFileSync(
     GIT_MODULES,
     `[submodule "web/content"]\n\tpath = web/content\n\turl = ${process.env.CMS_MODULE_URL}\n`
+);
+
+const submodule = spawn("git", [
+    "submodule",
+    "add",
+    "-f",
+    process.env.CMS_MODULE_URL,
+    "web/content",
+]);
+
+submodule.stdout.on("data", console.log);
+submodule.stderr.on("data", console.error);
+submodule.on("close", (code) =>
+    console.log(`Submodule child process exited with code ${code}`)
 );
